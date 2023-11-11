de1375ee2e546ddc1c7dd5f4f9b8e70cddc45996
herlambang k <herlambangk25@gmail.com>
2023-11-12 03:03:22 +0700

    fist commit

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script setup>
+const links = [{
+  label: 'Features',
+  to: '#features',
+  exactHash: true
+}, {
+  label: 'Documentation',
+  to: 'https://ui.nuxt.com/pro/guide',
+  target: '_blank'
+}]
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #logo>
+      Nuxt UI Pro
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+
+      <UButton to="https://github.com/nuxt-ui-pro/starter" target="_blank" icon="i-simple-icons-github" aria-label="GitHub" color="gray" variant="ghost" />
+    </template>
+  </UHeader>
+
+  <UMain>
+    <NuxtPage />
+  </UMain>
+
+  <UFooter>
+    <template #left>
+      Copyright © {{ new Date().getFullYear() }}
+    </template>
+  </UFooter>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ULandingHero
+      title="Nuxt UI Pro Starter"
+      description="Welcome to the minimal starter of Nuxt UI Pro, Nuxt UI Pro is a collection of premium components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes."
+    >
+      <template #links>
+        <UButton to="#features" size="lg" icon="i-heroicons-rocket-launch"> Get Started </UButton>
+
+        <UButton
+          to="https://ui.nuxt.com/pro/guide"
+          size="lg"
+          color="gray"
+          icon="i-heroicons-book-open"
+        >
+          Documentation
+        </UButton>
+      </template>
+    </ULandingHero>
+
+    <ULandingSection
+      id="features"
+      title="Features"
+      description="An advanced set of features to let you build anything."
+    >
+      <UPageGrid>
+        <ULandingCard
+          title="First Card"
+          description="This is my first description"
+          color="primary"
+        />
+        <ULandingCard
+          title="Second Card"
+          description="This is my second description"
+          color="fuchsia"
+        />
+        <ULandingCard title="Third Card" description="This is my third description" color="amber" />
+      </UPageGrid>
+    </ULandingSection>
+  </div>
+</template>
